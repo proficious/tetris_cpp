e3473f4833419dfc3c8892edc54626fe5a48216a
Paarth Raj Chauhan <58694722+paarthrc@users.noreply.github.com>
2020-11-19 12:25:58 +0530

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,21 +1,11 @@
 /*****************************************************************************************
 /* File: Game.cpp
 /* Desc: General class for the game
-/*
-/* gametuto.com - Javier López López (javilop.com)
+
+/* github.com/paarthrc - Paarth Raj Chauhan 
+/* gametuto.com - Javier LÃ³pez LÃ³pez (javilop.com)
 /*
 /*****************************************************************************************
-/*
-/* Creative Commons - Attribution 3.0 Unported
-/* You are free:
-/*	to Share — to copy, distribute and transmit the work
-/*	to Remix — to adapt the work
-/*
-/* Under the following conditions:
-/* Attribution. You must attribute the work in the manner specified by the author or licensor 
-/* (but not in any way that suggests that they endorse you or your use of the work).
-/*
-/*****************************************************************************************/
 
 // ------ Includes -----
 #ifndef LINUX
@@ -23,11 +13,8 @@
 #endif
 #include "Game.h"
 
-
-/* 
-======================================									
+/* 									
 Init
-====================================== 
 */
 Game::Game(Board *pBoard, Pieces *pPieces, IO *pIO, int pScreenHeight) 
 {
